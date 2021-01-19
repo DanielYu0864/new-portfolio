39858d93465479b866ffd69403d507e297b22f65
Daniel.Yu <adam741963@gmail.com>
2021-01-18 19:36:37 -0800

    header 0.1

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -10,7 +10,7 @@ function Header() {
           I am Daniel A. Yu
         </h1>
         <p>
-          a lifelong learner of knowledge and current Boot Camp student who presently has sight set on a fullstacck-developer position.
+          A lifelong learner of knowledge who presently has sight set on a fullstacck-developer position.
         </p>
       </div>
       <br/>
