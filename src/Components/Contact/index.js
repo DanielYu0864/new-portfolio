8bbdb211516bf3a9d5ba4a35a28338b22bbac261
Daniel.Yu <adam741963@gmail.com>
2021-04-15 11:29:30 -0700

    media position and contact position

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -4,16 +4,27 @@ function Contact() {
   return (
     <div className='contact' id='contact'>
     <div className="line"></div>
+      {/* <ul>
+        Contact Me:
+        <li>Phone: <a href="tel:+14256254073">425-625-4073</a></li>
+        <li>Email: <a href="mailto:adam741963@gmail.com">adam741963@gmail.com</a></li>
+        <li>Linkedin: <a target='blank' href="https://www.linkedin.com/in/daniel-yu-264360199/">Daniel Yu</a></li>
+        <li>Github: <a target='blank' href="https://github.com/DanielYu0864">DanielYu0864</a></li>
+        <li>Resume: <a target='blank' href="https://docs.google.com/document/d/1UuztsW4WQRWzihy84wIKtwEpPHqmhGGpWV4snOjjDj8/edit?usp=sharing">Google docs</a></li>
+      </ul> */}
+
+    <div className='info'>
       <ul>
         Contact Me:
-        <p>Phone: <a href="tel:+14256254073">425-625-4073</a></p>
-        <p>Email: <a href="mailto:adam741963@gmail.com">adam741963@gmail.com</a></p>
-        <p>Linkedin: <a target='blank' href="https://www.linkedin.com/in/daniel-yu-264360199/">Daniel Yu</a></p>
-        <p>Github: <a target='blank' href="https://github.com/DanielYu0864">DanielYu0864</a></p>
-        <p>Resume: <a target='blank' href="https://docs.google.com/document/d/1UuztsW4WQRWzihy84wIKtwEpPHqmhGGpWV4snOjjDj8/edit?usp=sharing">Google docs</a></p>
+        <li>Phone: <a href="tel:+14256254073">425-625-4073</a></li>
+        <li>Email: <a href="mailto:adam741963@gmail.com">adam741963@gmail.com</a></li>
+        <li>Linkedin: <a target='blank' href="https://www.linkedin.com/in/daniel-yu-264360199/">Daniel Yu</a></li>
+        <li>Github: <a target='blank' href="https://github.com/DanielYu0864">DanielYu0864</a></li>
+        <li>Resume: <a target='blank' href="https://docs.google.com/document/d/1UuztsW4WQRWzihy84wIKtwEpPHqmhGGpWV4snOjjDj8/edit?usp=sharing">Google docs</a></li>
       </ul>
-    <div className="line"></div>
+    </div>
 
+    <div className="line"></div>
     </div>
   )
 }
--- src/Components/Portfolio/Modal.js
+++ src/Components/Portfolio/Modal.js
@@ -6,11 +6,11 @@ function Modal(props) {
   return (
     <div className='project-container'>
       <h2>{title}</h2>
-      <div className='category'>Outline: <p>{category}</p></div>
+      <div className='category'>Project Summary: <p>{category}</p></div>
       <div className='language'>Language: <p>{language}</p></div>
       <div className='a-tags'>
-        <a className='link underline1' target='_blank' href={repo}>Repository</a>
-        <a className='link underline2' target='_blank' href={deploy ? deploy : repo}>Deploy</a>
+        <a className='link underline1' target='_blank' href={repo}>Github-Repository</a>
+        <a className='link underline2' target='_blank' href={deploy ? deploy : repo}>Deployed-Application</a>
       </div>
 
       {/* <Image
